9c2ec8c7fed68d12ead2518f9a830794e50e258c
hckme4 <hckme4@protonmail.com>
2024-06-02 22:26:51 -0400

    Updated build system to work with files, introduced linker and have basic kernel code. 	renamed:    buildenv/Dockerfile -> Dockerfile 	modified:   README.md 	deleted:    buildenv/notes.txt 	modified:   src/boot/boot.s 	new file:   src/boot/kernel/kernel.c 	new file:   src/boot/linker.ld

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ShrokOS
 OS Development!
+
+
+## The Build Environment
+To use the build environment, you will need docker, and run `docker run -it --rm $(docker build -q .)` to use the shell.
+This will launch an interactive shell with the complete i686-elf-gcc toolchain courtesy of user kevincharm: [https://github.com/kevincharm/i686-elf-gcc-toolchain](https://github.com/kevincharm/i686-elf-gcc-toolchain)
+
+From here, i686-elf-gcc and grub-mkrescue are made available in an ubuntu shell for building.
--- src/boot/boot.s
+++ src/boot/boot.s
@@ -1,44 +1,59 @@
-; Constants for multiboot header
-.set ALIGN,     1<<0                 ; align all loaded modules on page boundaries
-.set MEMINFO,   1<<1                 ; memory map
-.set FLAGS,     ALIGN | MEMINFO      ; multiboot 'flag' field
-.set MAGIC,     0x1BADB002           ; 'magic number' to find the header
-.set CHECKSUM,  -(MAGIC + FLAGS)     ; checksum all this as proof we are multiboot
+/* constants for the multiboot header. */
+.set ALIGN,    1<<0             /* align loaded modules on page boundaries */
+.set MEMINFO,  1<<1             /* provide memory map */
+.set FLAGS,    ALIGN | MEMINFO  /* this is the Multiboot 'flag' field */
+.set MAGIC,    0x1BADB002       /* 'magic number' lets bootloader find the header */
+.set CHECKSUM, -(MAGIC + FLAGS) /* checksum of above, to prove we are multiboot */
 
-; Declare multiboot header to mark program as a kernel
+/* 
+Declare multiboot header to mark program as a kernel
+*/
 .section .multiboot
 .align 4
 .long MAGIC
 .long FLAGS
 .long CHECKSUM
 
-; since value of esp is not defined, kernel must provide a stack.
+/*
+Since value of esp is not defined, kernel must provide a stack
+*/
 .section .bss
 .align 16
 stack_bottom:
-.skip 16384 ;16 KiB
+.skip 16384 # 16 KiB
 stack_top:
 
-; linker script to specify _start as entry to kernel. bootloader will jump here
-; when kernel has been loaded. no need to ret since bootloader has been unloaded
+/*
+linker script to specify _start as entry to kernel. bootloader will jump here when
+kernel has been loaded. no need to ret since bootloader has been unloaded.
+*/
 .section .text
 .global _start
 .type _start, @function
 _start:
-	; now loaded into 32-bit protected mode. must set up stack for C later on.
+	/*
+	now loaded into 32 bit protected mode, must set up stack for C later on.
+	*/
 	mov $stack_top, %esp
 
-	; THIS PLACE IS A GOOD SPOT TO INIT THE CPU STATE BEFORE ENTERING HIGH-LEVEL
-	; KERNEL CODE!!!
+	/*
+	THIS PLACE IS GOOD SPOT TO INIT CPU STATE BEFORE ENTERING HIGH-LEVEL KERNEL CODE
+	*/
 
-	; Enter the high level kernel, depending on the stack we init'ed earlier.
+	/*
+	Enter high level kernel, depending on initialized stack performed earlier
+	*/
 	call kernel_main
 
-	;if nothing else, disable interrupts, HLT computer and force jumping to HLT if skipped
+	/*
+	if system has nothing else, disable interrupts, HLT, and force jumping to HLT if skipped.
+	*/
 	cli
 1:	hlt
 	jmp 1b
 
-; set size of _start symbol to current location '.' minus its start for debugging and call tracing
+/*
+Set the size of the _start symbol to the current location '.' minus its start.
+This is useful when debugging or when you implement call tracing.
+*/
 .size _start, . - _start
-
